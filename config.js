629c172a176ed7ce9dd6f5af3825455ae4c36a4a
LstHeart <1042138926@qq.com>
2019-09-06 02:14:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
         title: `Mayne's Blog`, //网站标题
         pageSize: 3, // 每页多少篇文章
         description: `All things about Mayne`, // 网站描述 seo
-        siteUrl: `https://gine.me`, // 站点URL
+        siteUrl: `https://lstheart.top`, // 站点URL
         aboutPostSlug: `10a70927d25a43d19acf14e0d36354e7`, //没有单独的about页面，about页面指向文章。
         avatar: '41546411364_.pic.jpg', // src/static 目录下
         // 社交帐号配置，显示在页面最下方。
@@ -101,7 +101,7 @@ module.exports = {
     // seo 
     seo: {
         open: false, // 如果部署在netlify上，为了更好的seo，请开启此选项
-        siteUrl: 'https://gine.me', // 站点url
+        siteUrl: 'https://lstheart.top', // 站点url
         netlifyUrl: 'https://lstheart.netlify.com' //netlify分配的url
     },
 
